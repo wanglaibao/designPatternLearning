e2e1808dccbbc549ad2c4c81cc9e59a3e3766edb
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-23 10:20:00 +0800

    commit new java files info

---
+++ core-java-designpattern/src/main/java/com/laibao/core/java/designpattern/creation/singleton/StaticBlockSingleton.java
@@ -0,0 +1,22 @@
+package com.laibao.core.java.designpattern.creation.singleton;
+
+/**
+ * 静态初始化块单例
+ */
+public class StaticBlockSingleton {
+
+    private static final StaticBlockSingleton INSTANCE;
+
+    //JVM价值的时候就执行这个操作是最早的比类的实例化还要早
+    static {
+        INSTANCE = new StaticBlockSingleton();
+    }
+
+    private StaticBlockSingleton() {
+
+    }
+
+    public StaticBlockSingleton getInstance() {
+        return INSTANCE;
+    }
+}
